cebecb249caec99a3105f21cd1f2e4e2753fd12d
happiness1960 <klaxichapinex@gmail.com>
2024-11-21 11:22:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let name = "Jack";
+
+let FirstName = "Matthew"
+
+let LastName = "Peter"
+
+console.log(FirstName)
+
+let FullName = FirstName + " " + LastName
+
+console.log(FullName)
+
+// let char = prompt ('compose your text');
+
+// let charlength = char.length
+
+// alert ('you have written ' +  char.length  + '   character')
+
+console.log(FullName.toUpperCase ())
+console.log(FullName.toLowerCase ())
+
+console.log(FirstName.indexOf ('w'))
